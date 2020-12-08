19fa1a8caf200656ddfbc8c48cd10a2884702f15
Choua Cha <chouac@live.com>
2020-12-08 17:04:04 -0600

    Validation added to customer forms

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -8,20 +8,22 @@ namespace Vidly.Models
 {
     public class Customer
     {
-        public int Id { get; set; }
-        [Required]
+        public int? Id { get; set; }
+        
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
-
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? DateOfBirth { get; set; }
 
         public bool isSubscripedToNewsLietter { get; set; }
 
         public MembershipType MembershipType { get; set; }
+        [Required(ErrorMessage = "Please select a membership type.")]
         [Display(Name = "Membership Type")]
-        public byte MembershipTypeId { get; set; }
+        public byte? MembershipTypeId { get; set; }
 
     }
 }
